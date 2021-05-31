38433428b4939b31353657c185cedc45ab71847e
Hector Aristy Castillo Mateo <hectoristy@hotmail.com>
2021-05-31 00:06:12 -0400

    asi es que van

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,16 +1,24 @@
 //database
 const mysql = require('mysql');
 
-const db = mysql.createConnection({
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE
-});
-
 exports.login = async (req, res)=>{
     console.log(req.body);
     try {
+
+        const db = mysql.createConnection({
+            host: process.env.DATABASE_HOST,
+            user: process.env.DATABASE_USER,
+            password: process.env.DATABASE_PASSWORD,
+            database: process.env.DATABASE
+        });
+        
+        db.connect((error)=>{
+          if(error){
+            console.log(error);
+          }else{
+            console.log('Database Conected')
+          }
+        });
 
         const {username, password} = req.body;
         
